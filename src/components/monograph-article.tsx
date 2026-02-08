c6ad928f3917d25d007bc65a1182521cf4c24282
rodj6 <jeremysemails@gmail.com>
2026-02-07 19:31:47 -0800

    Update monograph-article.tsx

--- src/components/monograph-article.tsx
+++ src/components/monograph-article.tsx
@@ -6,7 +6,7 @@ import ManualComponent from "../../content/manual/pages/monograph.mdx";
 
 export default function MonographArticle() {
   return (
-    <div className="prose dark:prose-invert max-w-none">
+    <div className="prose dark:prose-invert max-w-none prose-pre:text-foreground prose-pre:opacity-100 prose-code:text-foreground">
       <ManualComponent components={mdxComponents} />
     </div>
   );
