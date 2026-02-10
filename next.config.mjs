1ec06d7a2d6840210b95a833780ff8804aae0333
rodj6 <jeremysemails@gmail.com>
2026-02-09 17:34:11 -0800

    Add remark-math and rehype-katex to MDX config for KaTeX

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 import createMDX from "@next/mdx";
+import remarMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins:[remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
 
 /** @type {import('next').NextConfig} */
